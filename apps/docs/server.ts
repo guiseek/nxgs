fb3229181e92b762b1e8b78e3ad67cd0cedf42b6
Gui Seek <guiseek@gmail.com>
2024-01-03 04:05:22 -0300

    style: 💄 lint fixes

--- apps/docs/server.ts
+++ apps/docs/server.ts
@@ -46,6 +46,7 @@ export function app(): express.Express {
   return server;
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function run(): void {
   const port = process.env['PORT'] || 4000;
 
