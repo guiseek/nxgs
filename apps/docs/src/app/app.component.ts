304f6a286242677125603a4a4f1e27a0da8ca0ab
Gui Seek <guiseek@gmail.com>
2024-01-04 05:17:50 -0300

    docs: ✏️ improvements

--- apps/docs/src/app/app.component.ts
+++ apps/docs/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MenuComponent } from './menu/menu.component';
 
 @Component({
   standalone: true,
   selector: 'nxgs-root',
-  imports: [RouterModule, MatToolbarModule],
+  imports: [RouterModule, MatToolbarModule, MenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- apps/docs/src/app/app.routes.ts
+++ apps/docs/src/app/app.routes.ts
@@ -1,42 +1,48 @@
 import { Route } from '@angular/router';
-import { HomeComponent } from './pages/home';
 import {
+  HomeComponent,
+  NestComponent,
+  AngularComponent,
+  GeneratorsComponent,
   CdkAccordionComponent,
   CdkDialogComponent,
   CdkTreeComponent,
-  GeneratorsComponent,
-} from './pages/angular';
+} from './pages';
 
 export const appRoutes: Route[] = [
   {
     path: '',
+    title: 'Home',
     component: HomeComponent,
   },
   {
-    path: 'angular/generators',
-    component: GeneratorsComponent,
-    children: [
-      {
-        path: 'accordion',
-        component: CdkAccordionComponent,
-      },
-      {
-        path: 'tree',
-        component: CdkTreeComponent,
-      },
-      {
-        path: 'dialog',
-        component: CdkDialogComponent,
-      },
-      {
-        path: '',
-        pathMatch: 'full',
-        redirectTo: 'accordion',
-      },
-    ],
+    path: 'angular',
+    title: 'Angular',
+    component: AngularComponent,
   },
   {
-    path: 'angular',
-    redirectTo: 'angular/generators',
+    path: 'nest',
+    title: 'Nest',
+    component: NestComponent,
+  },
+  {
+    path: 'generators',
+    title: 'Angular Generators',
+    component: GeneratorsComponent,
+  },
+  {
+    path: 'angular/generators/accordion',
+    title: 'CDK Accordion',
+    component: CdkAccordionComponent,
+  },
+  {
+    path: 'angular/generators/tree',
+    title: 'CDK Tree',
+    component: CdkTreeComponent,
+  },
+  {
+    path: 'angular/generators/dialog',
+    title: 'CDK Diaog',
+    component: CdkDialogComponent,
   },
 ];
