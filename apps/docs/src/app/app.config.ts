1f6a8360581ff847be504754da0827d6577b53b2
Gui Seek <guiseek@gmail.com>
2024-01-05 06:39:00 -0300

    docs: ✏️ add page transition effect

--- apps/docs/src/app/app.config.ts
+++ apps/docs/src/app/app.config.ts
@@ -1,13 +1,46 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, inject } from '@angular/core';
+import {
+  Router,
+  provideRouter,
+  withViewTransitions,
+  withInMemoryScrolling,
+  createUrlTreeFromSnapshot,
+} from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideAnimations(),
     provideClientHydration(),
-    provideRouter(appRoutes),
+    provideRouter(
+      appRoutes,
+      withInMemoryScrolling({
+        anchorScrolling: 'enabled',
+        scrollPositionRestoration: 'enabled',
+      }),
+      withViewTransitions({
+        onViewTransitionCreated: ({ transition, to }) => {
+          const router = inject(Router);
+          const toTree = createUrlTreeFromSnapshot(to, []);
+          // Skip the transition if the only thing changing is the fragment and queryParams
+          if (
+            router.isActive(toTree, {
+              paths: 'exact',
+              matrixParams: 'exact',
+              fragment: 'ignored',
+              queryParams: 'ignored',
+            })
+          ) {
+            transition.skipTransition();
+          }
+        },
+      })
+    ),
+    provideClientHydration(),
+    provideHttpClient(withFetch()),
+    provideAnimations(),
   ],
 };
