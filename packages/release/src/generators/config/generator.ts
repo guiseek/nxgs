fd8f466987fabf2c196699c5f87290c51f2b7f3d
Gui Seek <guiseek@gmail.com>
2024-01-19 01:37:56 -0300

    fix(release): fix executor release config

--- packages/release/src/generators/config/generator.ts
+++ packages/release/src/generators/config/generator.ts
@@ -1,23 +1,17 @@
 import {
   Tree,
-  readJson,
   workspaceRoot,
   readProjectConfiguration,
   updateProjectConfiguration,
 } from '@nx/devkit';
-import type { PackageJson } from 'nx/src/utils/package-json';
 import { replaceOptions } from '../../lib/utilities';
 import type { ConfigOptions } from './schema';
-import { join, relative } from 'node:path';
-
-const root = join(__dirname, '..', '..', '..');
+import { relative } from 'node:path';
 
 export async function configGenerator(
   tree: Tree,
   { projectName, ...options }: ConfigOptions
 ) {
-  const { executors } = readJson(tree, join(root, 'executors.json'));
-
   const projectConfig = readProjectConfiguration(tree, projectName);
 
   const semanticReleaseConfig = replaceOptions(options, {
@@ -27,15 +21,12 @@ export async function configGenerator(
     workspaceDir: workspaceRoot,
   });
 
-  const { name } = readJson<PackageJson>(tree, join(root, 'package.json'));
-  const [executor] = Object.keys(executors) ?? [];
-
   updateProjectConfiguration(tree, projectName, {
     ...projectConfig,
     targets: {
       ...projectConfig.targets,
       release: {
-        executor: `${name}:${executor}`,
+        executor: `@nxgs/release:release`,
         options: semanticReleaseConfig,
       },
     },
