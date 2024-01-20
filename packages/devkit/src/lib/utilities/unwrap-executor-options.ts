923a47ace6e468fb80d8c35c4eb8a15b3d6f17f0
Gui Seek <guiseek@gmail.com>
2024-01-20 09:43:05 -0300

    chore(devkit): remove utility files

--- packages/devkit/src/lib/utilities/unwrap-executor-options.ts
+++
@@ -1,9 +0,0 @@
-import { ExecutorOptions } from '../types';
-
-export function unwrapExecutorOptions<T>(options: ExecutorOptions<T>) {
-  if (options && typeof options === 'object' && 'executor' in options) {
-    return options.options;
-  }
-
-  return options as T;
-}
